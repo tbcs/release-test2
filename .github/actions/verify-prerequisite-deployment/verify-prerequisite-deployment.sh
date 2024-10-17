b624224162ec78462f2eeeb5f0906c61fa2426c4
Tibor Csögör <tibi@tiborius.net>
2024-10-17 18:01:21 +0200

    add precondition

---
+++ .github/actions/verify-prerequisite-deployment/verify-prerequisite-deployment.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -eu
+
+# DEPLOYMENT_ENVIRONMENT="prod"
+# PREREQUISITE_ENVIRONMENT="stage"
+# VERSION="aff43153"
+# #VERSION="xaff43153"
+# GITHUB_REPOSITORY="bettermile/bm-route-sequencer"
+
+# ----
+
+repo_owner=$(echo "${GITHUB_REPOSITORY}" | cut -d'/' -f1)
+repo_name=$(echo "${GITHUB_REPOSITORY}" | cut -d'/' -f2)
+
+jq_query='
+.data.repository.deployments.nodes
+| map(select(.latestStatus.state == "INACTIVE" or .latestStatus.state == "SUCCESS"))
+| map(select(.commitOid | startswith($commit_prefix)))
+| first | .createdAt // ""
+'
+
+graphql_query='
+query($repoOwner: String!, $repoName: String!, $environment: String!) {
+  repository(owner: $repoOwner, name: $repoName) {
+    deployments(first: 100,
+                environments: [$environment],
+                orderBy: {field: CREATED_AT, direction: DESC}) {
+      nodes {
+        id
+        createdAt
+        latestStatus {
+          state
+        }
+        commitOid
+      }
+    }
+  }
+}
+'
+
+latest_deployment=$(gh api graphql \
+  -F repoOwner="$repo_owner" \
+  -F repoName="$repo_name" \
+  -F environment="$DEPLOYMENT_ENVIRONMENT" \
+  -f query="$graphql_query" \
+  | jq -r --arg commit_prefix "$VERSION" "$jq_query")
+
+if [ -n "$latest_deployment" ]; then
+  echo "::notice::Version '$VERSION' has been previously deployed to" \
+       "environment '$DEPLOYMENT_ENVIRONMENT' at $latest_deployment."
+  exit 0
+else
+  echo "::error::Deployment aborted:" \
+       "deployment to '$DEPLOYMENT_ENVIRONMENT' requires a successful prior deployment" \
+       "to '$PREREQUISITE_ENVIRONMENT', but none was found."
+  exit 1
+fi
