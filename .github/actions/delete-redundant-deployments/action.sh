cb4a3b3786f93faf0b5c04cc1c8ac02aa0d6c03e
Tibor Csögör <tibi@tiborius.net>
2024-10-18 01:11:08 +0200

    add del

---
+++ .github/actions/delete-redundant-deployments/action.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -eu
+
+repo_owner=$(echo "${GITHUB_REPOSITORY}" | cut -d'/' -f1)
+repo_name=$(echo "${GITHUB_REPOSITORY}" | cut -d'/' -f2)
+
+graphql_query='
+query ($repoOwner: String!, $repoName: String!, $environment: String!) {
+  repository(owner: $repoOwner, name: $repoName) {
+    deployments(
+      first: 100
+      environments: [$environment]
+      orderBy: {field: CREATED_AT, direction: DESC}
+    ) {
+      nodes {
+        databaseId
+        createdAt
+        latestStatus {
+          state
+        }
+        commitOid
+        payload
+      }
+    }
+  }
+}
+'
+
+jq_query='
+.data.repository.deployments.nodes
+| map(select(.latestStatus.state == "INACTIVE"))
+| map(select(.commitOid | startswith($version)))
+| map(select((.payload // "") | contains($deploymentPayloadMarker) | not))
+| [ .[] | .databaseId ] | @tsv
+'
+
+deployment_ids=$(gh api graphql \
+  -F repoOwner="$repo_owner" \
+  -F repoName="$repo_name" \
+  -F environment="$DEPLOYMENT_ENVIRONMENT" \
+  -f query="$graphql_query" \
+  | jq -r \
+    --arg version "$VERSION" \
+    --arg deploymentPayloadMarker "$DEPLOYMENT_PAYLOAD_MARKER" "$jq_query")
+
+for deployment_id in $deployment_ids; do
+  gh api --silent -X DELETE "/repos/{owner}/{repo}/deployments/${deployment_id}"
+  echo "::notice::Redundant GitHub deployment object '${deployment_id}' was deleted."
+done
